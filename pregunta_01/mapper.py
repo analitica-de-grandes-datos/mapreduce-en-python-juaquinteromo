cced882814752ac580f75a9cb1c079cd383b95aa
juaquinteromo <juaquinteromo@unal.edu.co>
2023-05-16 18:42:18 -0500

    PR
    PREGUNTA1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+# Iterar sobre cada línea de entrada
+for line in sys.stdin:
+    # Eliminar espacios en blanco y dividir la línea en campos
+    fields = line.strip().split(',')
+
+    # Comprobar si la línea contiene todos los campos necesarios
+    if len(fields) == 21:
+        # Obtener el valor del atributo "credit_history"
+        credit_history = fields[4]
+
+        # Emitir una clave-valor para el atributo "credit_history"
+        print(credit_history + '\t' + '1')
--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,3 +1,31 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+current_key = None
+current_count = 0
+
+# Iterar sobre cada línea de entrada
+for line in sys.stdin:
+    # Eliminar espacios en blanco y dividir la línea en clave y valor
+    key, value = line.strip().split('\t')
+
+    # Convertir el valor en entero
+    count = int(value)
+
+    # Si la clave es la misma que la anterior, sumar el valor
+    if key == current_key:
+        current_count += count
+    else:
+        # Si la clave es diferente, imprimir la clave anterior y su conteo
+        if current_key:
+            print(current_key + '\t' + str(current_count))
+
+        # Actualizar la clave y el conteo
+        current_key = key
+        current_count = count
+
+# Imprimir la última clave y su conteo
+if current_key:
+    print(current_key + '\t' + str(current_count))
