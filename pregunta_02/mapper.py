8afd08eed7a5afb249685989196e4967c570dca8
juaquinteromo <juaquinteromo@unal.edu.co>
2023-05-19 16:01:01 -0500

    voy volando

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+# leo las dos columnas implicadas: 
+import sys
+if __name__ == "__main__":
+    for linea in sys.stdin:
+        columnas = linea.split(",")
+        columna_purpose = columnas[3]
+        columna_amount = columnas[4]
+        sys.stdout.write(f"{columna_purpose}\t{columna_amount}\n")
