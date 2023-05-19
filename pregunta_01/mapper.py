69b58bf73957cac9619ea964348842b2e8dff63f
juaquinteromo <juaquinteromo@unal.edu.co>
2023-05-19 15:11:37 -0500

    pregunta1
    pg1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -2,16 +2,12 @@
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 import sys
-
+if __name__ == '__main__':
 # Iterar sobre cada línea de entrada
-for line in sys.stdin:
+    for linea in sys.stdin:
     # Eliminar espacios en blanco y dividir la línea en campos
-    fields = line.strip().split(',')
-
-    # Comprobar si la línea contiene todos los campos necesarios
-    if len(fields) == 21:
-        # Obtener el valor del atributo "credit_history"
-        credit_history = fields[4]
-
-        # Emitir una clave-valor para el atributo "credit_history"
-        print(credit_history + '\t' + '1')
+        campos = linea.split(',')
+        campos_credit = campos[2]
+        sys.stdout.write(f"{campos_credit}\n")
+#
+    
