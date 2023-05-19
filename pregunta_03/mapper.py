f504d9bf5a492c6aaa0405c525bfcd8a4f53d064
juaquinteromo <juaquinteromo@unal.edu.co>
2023-05-19 17:00:46 -0500

    vamos que vamos

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -4,10 +4,12 @@
 
 
 import sys
+
 if __name__ == "__main__":
-    for linea in sys.stdin:
-        columnas = linea.split(",")
+
+    for line in sys.stdin:
+        columnas= line.split(",")      
         if len(columnas) >= 2:
-            columna_letra = columnas[0]
-            columna_numero = columnas[1]
-            sys.stdout.write(f"{columna_letra}\t{columna_numero}\n")
+            columna1 = columnas[0]
+            columna2 = int(columnas[1])
+            sys.stdout.write(f"{columna1}\t{columna2}\n")
--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -5,11 +5,12 @@ import sys
 if __name__ == '__main__':
     datos = []
     for line in sys.stdin:
-        columnas = line.strip().split("\t") #\t eson tabs
+        columnas = line.strip().split("\t")
         columna1 = columnas[0]
         columna2 = int(columnas[1])
         datos.append((columna1,columna2))
-    datos_ordenados = sorted(datos, key=lambda x: int(x[1]))
+    datos= sorted(datos,key=lambda x: x[1])
     
-    for item in datos_ordenados:
+    for item in datos:
         sys.stdout.write(f"{item[0]},{item[1]}\n")
+
