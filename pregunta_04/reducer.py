9a9225a98acf7cf0f3495fa46a85a0e4d3ccae9c
juaquinteromo <juaquinteromo@unal.edu.co>
2023-05-19 17:24:00 -0500

    moral

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    con_letras = {}
+    for line in sys.stdin:
+        columnas = line.split("\n")
+        letras = columnas [0]
+        if letras in con_letras:
+           con_letras[letras] += 1
+        else:
+            con_letras[letras] = 1
+    ord_con_letras = sorted(con_letras.items())
+    for key,valor in ord_con_letras:
+        sys.stdout.write(f"{key},{valor}\n")
