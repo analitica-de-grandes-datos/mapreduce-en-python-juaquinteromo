81b267c5a52c5d5dc0f689c295291aa0778302f6
juaquinteromo <juaquinteromo@unal.edu.co>
2023-05-26 16:55:57 -0500

    Update mapper.py

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,6 +1,7 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
-#import sys
+#
+import sys
 if __name__ == "__main__":
     for linea in sys.stdin:
         columnas = linea.strip().split('   ')
